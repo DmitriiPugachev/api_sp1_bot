ed6a0251157c0a8a1385ea6a3de0e154eeb45437
DmitriiPugachev <pugachev.feddebate@gmail.com>
2021-07-03 14:09:31 +0200

    Logging and some changing in parsing process added.

--- homework.py
+++ homework.py
@@ -1,15 +1,37 @@
+import logging
 import os
 import time
+from logging.handlers import RotatingFileHandler
+
 import requests
 import telegram
 from dotenv import load_dotenv
-
 
 load_dotenv()
 
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+SLEEP_LENGTH = 5 * 60
+
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename='main.log',
+    filemode='a',
+    format='%(asctime)s, %(levelname)s, %(message)s, %(name)s',
+)
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler = RotatingFileHandler(
+    'homework.log',
+    mode='a',
+    maxBytes=50000000,
+    backupCount=5
+)
+logger.addHandler(handler)
 
 # проинициализируйте бота здесь,
 # чтобы он был доступен в каждом нижеобъявленном методе,
@@ -17,13 +39,31 @@ CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
 
+def send_message(message):
+    return bot.send_message(CHAT_ID, message)
+
+
 def parse_homework_status(homework):
-    homework_name = homework['homework_name']
-    homework_status = homework['status']
+    try:
+        homework_name = homework['homework_name']
+        homework_status = homework['status']
+    except TypeError as error:
+        logger.error(error, exc_info=True)
+        error_message = f'TypeError in parse_homework_status: {error}'
+        send_message(error_message)
+    except KeyError as error:
+        logger.error(error, exc_info=True)
+        error_message = f'KeyError in parse_homework_status: {error}'
+        send_message(error_message)
     if homework_status == 'rejected':
         verdict = 'К сожалению, в работе нашлись ошибки.'
-    else:
+    elif homework_status == 'approved':
         verdict = 'Ревьюеру всё понравилось, работа зачтена!'
+    else:
+        verdict = (
+            'Упс, ошибочка вышла. Вашу работу еще не проверили. ',
+            'Но ее уже взяли в ревью, так что скоро проверят. ',
+        )
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
@@ -31,29 +71,45 @@ def get_homeworks(current_timestamp):
     url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
     headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
     payload = {'from_date': current_timestamp}
-    homework_statuses = requests.get(url, headers=headers, params=payload)
-    return homework_statuses.json()
-
-
-def send_message(message):
-    return bot.send_message(CHAT_ID, message)
+    try:
+        homework_statuses = requests.get(url, headers=headers, params=payload)
+    except requests.RequestException as error:
+        logger.error(error, exc_info=True)
+        error_message = f'RequestException in get_homeworks: {error}'
+        send_message(error_message)
+    try:
+        return homework_statuses.json()
+    except TypeError as error:
+        logger.error(error, exc_info=True)
+        error_message = f'TypeError in get_homeworks: {error}'
+        send_message(error_message)
+    except ValueError as error:
+        logger.error(error, exc_info=True)
+        error_message = f'ValueError in get_homeworks: {error}'
+        send_message(error_message)
 
 
 def main():
     current_timestamp = int(time.time())  # Начальное значение timestamp
+    time_before_sleep = current_timestamp - SLEEP_LENGTH
 
     while True:
+        logger.debug('Bot has been started.')
         try:
-            homeworks_from_api_answer = \
-                get_homeworks(current_timestamp)['homeworks']
+            homeworks_from_api_answer = (
+                get_homeworks(time_before_sleep)['homeworks']
+            )
             if len(homeworks_from_api_answer) > 0:
                 current_homework = homeworks_from_api_answer[0]
                 message = parse_homework_status(current_homework)
                 send_message(message)
-            time.sleep(5 * 60)  # Опрашивать раз в пять минут
+                logger.info('Message has been sent.')
+            time.sleep(SLEEP_LENGTH)  # Опрашивать раз в пять минут
 
-        except Exception as e:
-            print(f'Бот упал с ошибкой: {e}')
+        except Exception as error:
+            logger.error(error, exc_info=True)
+            error_message = f'Бот упал с ошибкой: {error}'
+            send_message(error_message)
             time.sleep(5)
 
 
